7b123ce8af6b7d9f9c26b964d94a531aac91850f
Michael Lyalin <mc.lyalin@gmail.com>
2022-03-14 18:33:35 +0500

    replace func with a class, add state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      members: [
+        {
+          name: 'Papa',
+          id: 'id1'
+        },
+        {
+          name: 'Mama',
+          id: 'id1'
+        },
+        {
+          name: 'Child1',
+          id: 'id1'
+        },
+        {
+          name: 'Child2',
+          id: 'id1'
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.members.map(member => <h1 key={member.id}> { member.name } </h1>)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
