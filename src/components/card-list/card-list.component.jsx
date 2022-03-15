8107354b1f4b9403873a7e59ae7acd691290a8c8
Michael Lyalin <mc.lyalin@gmail.com>
2022-03-15 18:58:05 +0500

    add card component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Card } from '../card/card.component';
 import './card-list.styles.css';
 
 export const CardList = (props) => (
-  <div className='card-list'>
-    {this.state.members.map(member => (
-      <h1 key={member.id}> {member.name} </h1>
+  <div className="card-list">
+    {props.members.map(member => (
+      <Card key={member.id} member={member} />
     ))}
   </div>
 );
